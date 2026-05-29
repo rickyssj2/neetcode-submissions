0ec53c056cdc40b287ac86a48350dc79f9c15dc9
Aryan Pandey <aryanpandey048@gmail.com>
2026-05-29 11:15:20 -0700

    Add: time-based-key-value-store - submission-4

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-4.py
@@ -0,0 +1,24 @@
+class TimeMap:
+
+    def __init__(self):
+        self.hmap = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.hmap[key].append((value, timestamp))
+
+    def get(self, key: str, timestamp: int) -> str:
+        values = self.hmap[key]
+        print(values)
+        l, r = 0, len(values) - 1
+
+        while l < r:
+            # mid = l + (r - l) // 2
+            mid = r - (r - l) // 2
+
+            if values[mid][1] <= timestamp:
+                l = mid
+            else:
+                r = mid - 1
+        
+        return values[r][0] if 0 <= r < len(values) and values[r][1] <= timestamp else ""
+
