a3081b83b66ffc31009705a4801d8c17eb3d34e2
Aryan Pandey <aryanpandey048@gmail.com>
2026-06-13 13:06:53 -0700

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.py
@@ -0,0 +1,14 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda x: x.start)
+        for io1, io2 in pairwise(intervals):
+            if io1.end > io2.start: return False
+        return True
