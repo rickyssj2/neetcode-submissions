85e0f5a07d75aac952ec4e07dfc94fe8fcbb113f
Aryan Pandey <aryanpandey048@gmail.com>
2026-06-20 10:17:47 -0700

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        ones = 0
+        while n:
+            n &= (n - 1)
+            ones += 1
+        return ones
+
+    def countBits(self, n: int) -> List[int]:
+        return [self.hammingWeight(i) for i in range(n + 1)]
