e4414f916a30489bf5c23800e8ae83809b0ccef1
Aryan Pandey <aryanpandey048@gmail.com>
2026-06-30 13:23:19 -0700

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.py
@@ -0,0 +1,30 @@
+from functools import cache
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # 1, 5, 7 total = 10
+        # dp[i] min number of coins needed to make total i
+        # dp[i] = min(1 + dp[i - 5], 1 + dp[i - 1], 1 + dp[i - 7])
+        # @cache
+        # def dp(amount):
+        #     if amount < 0: return float('inf')
+        #     if amount == 0: return 0
+
+        #     ans = float('inf')
+        #     for c in coins:
+        #         ans = min(ans, 1 + dp(amount - c))
+        #     return ans
+
+        # ret = dp(amount)
+
+        # return ret if ret != float('inf') else -1
+
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+
+        for i in range(amount + 1):
+            if dp[i] == float('inf'): continue
+
+            for c in coins:
+                if i + c <= amount: dp[i + c] = min(dp[i] + 1, dp[i + c])
+        
+        return dp[amount] if dp[amount] != float('inf') else -1
