8f99ed316b43d8ef3051e606e1a947df60321b90
Aryan Pandey <aryanpandey048@gmail.com>
2026-07-29 11:31:49 -0700

    Add: network-delay-time - submission-1

---
+++ Data Structures & Algorithms/network-delay-time/submission-1.py
@@ -0,0 +1,23 @@
+from math import inf
+from heapq import heappush, heappop
+# from collections import defaultdict
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # Dijkstra's Algo
+        # Bellman-Ford Algo
+
+        dist = [inf] * (n + 1)
+        dist[k] = 0
+        adj = defaultdict(list)
+
+        for u, v, w in times:
+            adj[u].append((v, w))
+
+        for i in range(n):
+            for node in range(1, n + 1):
+                for nei, w in adj[node]:
+                    dist[nei] = min(dist[nei], dist[node] + w)
+
+        ans = max(dist[1:])
+        return ans if ans != inf else -1
